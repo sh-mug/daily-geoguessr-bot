40dff86d1b02c3ad04a9a9c4f1d9abe073a60eeb
sh-mug <52068539+sh-mug@users.noreply.github.com>
2024-08-01 22:12:40 +0900

    [add] dump logs

--- src/geoguessr-api/highscores.ts
+++ src/geoguessr-api/highscores.ts
@@ -25,10 +25,23 @@ export const getHighscores = async (): Promise<ChallengeHighscores | undefined> 
         const options = createRequestOptions('GET', cookie);
         const response = await fetch(highscoresUrl(tokenData.token), options as any);
         console.log('Highscores fetched:', response.statusText);
+
+        // save token and response.json() to file
+        const dateStr = new Date().toISOString().split('T')[0];
+        const responseJson = await response.json();
+        await fs.mkdir('logs/challenge_ids', { recursive: true });
+        await fs.mkdir('logs/raw_challenge_results', { recursive: true });
+        await fs.writeFile(
+            path.resolve(`logs/challenge_ids/${dateStr}.json`),
+            tokenData.token, 'utf8');
+        await fs.writeFile(
+            path.resolve(`logs/raw_challenge_results/${dateStr}.json`),
+            JSON.stringify(responseJson), 'utf8');
+
         return {
             timestamp: tokenData.timestamp,
             token: tokenData.token,
-            highscores: await response.json()
+            highscores: responseJson
         } as ChallengeHighscores;
     } catch (error) {
         console.error('Error getting highscores:', error);
